308d98c3a6d174a48cd04485e831e253c05a5ed6
Tanapon Nanta <chroma@Tanapons-MacBook-Pro.local>
2020-11-27 15:17:30 +0700

    add bootstrap js

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,10 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js', crossorigin: 'anonymous' },
+      { src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js', crossorigin: 'anonymous' }
     ]
   },
 
