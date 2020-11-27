5e824f6b4805aa9bc8904de6808287eb5de36025
Tanapon Nanta <chroma@Tanapons-MacBook-Pro.local>
2020-11-27 16:07:52 +0700

    add logout btn

--- components/user/UserNavbar.vue
+++ components/user/UserNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <button
-      class="navbar-toggler position-fixed"
+      class="navbar-toggler"
       type="button"
       data-toggle="collapse"
       data-target="#userNavbarToggler"
@@ -14,6 +14,28 @@
       <img src="~/assets/logo.svg" alt="logo">
     </nuxt-link>
     <!-- Menus -->
-    <div id="userNavbarToggler" class="collapse navbar-collapse" />
+    <div id="userNavbarToggler" class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a href="#" class="nav-link" @click="signOut()">LogOut</a>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    signOut () {
+      this.$store.dispatch('user/signOut')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbar-toggler {
+  margin-right: -30px !important;
+}
+</style>
