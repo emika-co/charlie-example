f7df1aa0b7a49583fa7fb8c38c4b93ad457de9f3
Tanapon Nanta <chroma@Tanapons-MacBook-Pro.local>
2020-11-27 15:33:50 +0700

    navbar

---
+++ components/user/UserNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button
+      class="navbar-toggler position-fixed"
+      type="button"
+      data-toggle="collapse"
+      data-target="#userNavbarToggler"
+      aria-controls="userNavbarToggler"
+      aria-expanded="false"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <nuxt-link to="/" class="navbar-brand mx-auto">
+      <img src="~/assets/logo.svg" alt="logo">
+    </nuxt-link>
+    <!-- Menus -->
+    <div id="userNavbarToggler" class="collapse navbar-collapse" />
+  </nav>
+</template>
