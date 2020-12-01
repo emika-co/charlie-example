f0368b1d758eef3b7e3f753e34dbb0417dd9bb98
Tanapon Nanta <chroma@Tanapons-MacBook-Pro.local>
2020-12-01 14:35:27 +0700

    test env

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -60,16 +60,14 @@ export default {
   },
 
   env: {
-    // firebaseConfig: {
-    //   apiKey: process.env.FIREBASE_API_KEY,
-    //   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    //   databaseURL: process.env.FIREBASE_DATABASE_URL,
-    //   projectId: process.env.FIREBASE_PROJECT_ID,
-    //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    //   appId: process.env.FIREBASE_APP_ID,
-    //   measurementId: process.env.FIREBASE_MEASUREMENT_ID
-    // }
-    firebaseConfig: process.env.FIREBASE_CONFIG
+    firebaseConfig: {
+      apiKey: process.env.FIREBASE_API_KEY || 'API_KEY',
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN || 'PROJECT_ID.firebaseapp.com',
+      projectId: process.env.FIREBASE_PROJECT_ID || 'PROJECT_ID',
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'PROJECT_ID.appspot.com',
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || 'SENDER_ID',
+      appId: process.env.FIREBASE_APP_ID || 'APP_ID',
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID || 'G-MEASUREMENT_ID'
+    }
   }
 }
