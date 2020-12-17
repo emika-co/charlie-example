2c9a8f5be4ecbfa3fc1dfb7e2a62882f80ef9680
Tanapon Nanta <pangpond556_@hotmail.com>
2020-12-17 15:28:47 +0700

    store index

--- src/pages/sellers/stores/index.vue
+++ src/pages/sellers/stores/index.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    index
+  <div id="background" class="container d-flex justify-content-center">
+    <div class="align-self-center w-100 text-center">
+      <h4 class="title">
+        คุณเป็นผู้ขายแล้ว
+      </h4>
+      <p class="mx-5">
+        คุณสามารถลงขายสินค้าของคุณได้ทันที โดยการกดปุ่มด้านล่าง
+      </p>
+      <div class="col-8 mx-auto mt-4">
+        <nuxt-link to="/sellers/stores/items" class="btn btn-primary w-100">
+          เริ่มขายสินค้าเลย
+        </nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +21,17 @@ export default {
   middleware: ['auth', 'seller']
 }
 </script>
+
+<style scoped>
+  .container {
+    height: 100vh;
+  }
+  #background {
+    background-image: url('/_nuxt/assets/background.svg');
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+  .title {
+    margin-top: 380px;
+  }
+</style>
---
+++ src/pages/sellers/stores/items.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Item Page
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
