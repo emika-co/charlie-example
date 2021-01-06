fe1f27e91de47e4ea0910fe140f5d861f8cb1440
Tanapon Nanta <pangpond556_@hotmail.com>
2021-01-06 15:15:05 +0700

    fixed margin

--- src/pages/stores/_storeId/items/_itemId.vue
+++ src/pages/stores/_storeId/items/_itemId.vue
@@ -56,7 +56,7 @@
               </div>
               <hr class="mt-0">
               <div class="row">
-                <div class="col-12">
+                <div class="col-12 mb-2">
                   <div class="float-left mr-2">
                     <img src="~/assets/check.svg" class="green-checker">
                   </div>
