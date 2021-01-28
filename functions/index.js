d9120e3ef97c17f038ee9004c6ce8acc7b4732ba
Tanapon Nanta <pangpond556_@hotmail.com>
2021-01-28 16:37:53 +0700

    remove test func

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,6 @@ admin.initializeApp();
 const sellers = require('./sellers');
 const items = require('./items');
 const payments = require('./payments');
-const test = require('./test');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -20,5 +19,3 @@ exports.showItem = items.showItem;
 exports.updateItem = items.updateItem;
 exports.buyItem = items.buyItem;
 exports.paymentConfirmation = payments.paymentConfirmation;
-
-exports.test = test.test;
