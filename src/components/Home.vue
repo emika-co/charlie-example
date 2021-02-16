e3119d42efad4cdeded34d944d8486a954f2be8e
Tanapon Nanta <pangpond556_@hotmail.com>
2021-02-16 14:33:02 +0700

    device checker

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>
+      <div class="container d-flex justify-content-center vh100">
+        <div class="align-self-center w-100 text-center">
+          <div class="title">
+            <h4 class="mb-3">
+              Filolo
+            </h4>
+            <p>สมัครสมาชิก หรือเข้าสู่ระบบ</p>
+          </div>
+          <div class="spacer" />
+          <!-- <facebook-login class="mb-3" /> -->
+          <google-login />
+          <div>
+            <p class="mb-4">
+              <nuxt-link to="/" class="link">
+                Privacy Policy
+              </nuxt-link>
+              and
+              <nuxt-link to="/" class="link">
+                Terms of Service
+              </nuxt-link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <img class="img-fluid" src="https://firebasestorage.googleapis.com/v0/b/charlie-296709.appspot.com/o/resources%2Fbg%2Fbg-desktop.jpg?alt=media&token=109775e7-fab6-4475-93bc-38b23265d021">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.vh100 {
+  height: 100vh;
+}
+</style>
---
+++ src/components/HomeMobile.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container d-flex justify-content-center bg">
+    <div class="align-self-center w-100 text-center">
+      <div class="title">
+        <h4 class="mb-3">
+          Filolo
+        </h4>
+        <p>สมัครสมาชิก หรือเข้าสู่ระบบ</p>
+      </div>
+      <div class="spacer" />
+      <!-- <facebook-login class="mb-3" /> -->
+      <google-login />
+      <div class="fixed-bottom">
+        <p class="mb-4">
+          <nuxt-link to="/" class="link">
+            Privacy Policy
+          </nuxt-link>
+          and
+          <nuxt-link to="/" class="link">
+            Terms of Service
+          </nuxt-link>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import FacebookLogin from '~/components/auth/FacebookLogin.vue'
+import GoogleLogin from '~/components/auth/GoogleLogin.vue'
+export default {
+  middleware: ['home'],
+  components: { GoogleLogin }
+}
+</script>
+
+<style scoped>
+  .container {
+    height: 100vh;
+  }
+  .spacer {
+    margin-bottom: 9rem;
+  }
+  .title {
+    margin-top: -13.5rem;
+  }
+  .link {
+    color: white;
+    text-decoration: underline;
+  }
+</style>
